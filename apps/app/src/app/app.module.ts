b8b7906ae5cd7936e2e430d2f1f98352b26be515
Tungsten78 <3805338+Tungsten78@users.noreply.github.com>
2023-02-08 14:05:10 -0700

    app + lib + tailwind

--- apps/app/src/app/app.module.ts
+++ apps/app/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedUiComponent } from '@nx-integrated-angular/shared/ui';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule,SharedUiComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
